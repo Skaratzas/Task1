d272bfa3de6c5b4b49a95431f9227eba6e9b12f4
Stavros Karatzas <76097758+Skaratzas@users.noreply.github.com>
2020-12-30 13:38:46 +0100

    Add files via upload

---
+++ Task1.0.0.cs
@@ -0,0 +1,29 @@
+﻿using LibGit2Sharp;
+using System;
+
+namespace Task1._0._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Repository.Clone("https://github.com/Skaratzas/repository", @"C:\GitHub\repository");
+
+            Repository.Init(@"C:\GitHub\git");
+
+
+            using (var repository = new Repository(@"C:\GitHub\repository")) {
+                var commits = repository.Commits;
+                foreach (var commit in commits)
+                {
+                    Console.WriteLine($"{commit.Id.ToString().Substring(0, 6)} " +
+                                      $"{commit.Author.When.ToLocalTime()} " +
+                                      $"{commit.MessageShort} " +
+                                      $"{commit.Author.Name}");
+                }
+
+            }
+        }
+    }
+}
