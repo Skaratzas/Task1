b09239a74763b035906e7ddcc3c2d7683f94c4ef
Stavros Karatzas <76097758+Skaratzas@users.noreply.github.com>
2021-01-07 13:23:06 +0100

    Add files via upload

---
+++ Task1.0.1/NewBranch.cs
@@ -0,0 +1,20 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1._0._1
+{
+    class NewBranch
+    {
+        public NewBranch(Repository repository)
+        {
+            var branches = repository.Branches;
+            Console.WriteLine("\nBranches: ");
+            foreach (var branch in branches)
+            {
+                Console.WriteLine(branch.FriendlyName);
+            }
+        }
+    }
+}
---
+++ Task1.0.1/NewCommit.cs
@@ -0,0 +1,23 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1._0._1
+{
+    class NewCommit
+    {
+        public NewCommit(Repository repository)
+        {
+            var commits = repository.Commits;
+            Console.WriteLine("Commits: ");
+            foreach (var commit in commits)
+            {
+                Console.WriteLine($"{commit.Id.ToString().Substring(0, 6)} " +
+                                  $"{commit.Author.When.ToLocalTime()} " +
+                                  $"{commit.MessageShort} " +
+                                  $"{commit.Author.Name}");
+            }
+        }
+    }
+}
